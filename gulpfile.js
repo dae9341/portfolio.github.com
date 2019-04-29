1ef7bb5c966df8696bc3df759b3dc3d37f48349e
kdh976 <dae9341@gmail.com>
2019-04-29 18:00:31 +0900

    gulp 세팅

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require("gulp"); //
+var concat = require("gulp-concat"); //
+var uglify = require("gulp-uglify"); //
+var sass = require("gulp-sass"); //sass 컴파일
+
+var src = {};
+src.root = "/public/";
