b3a78a25e1b95515300706fd8025920de52b52cd
kdh976 <dae9341@gmail.com>
2019-04-30 17:47:49 +0900

    gulp 세팅

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,50 @@
-var gulp = require("gulp"); //
-var concat = require("gulp-concat"); //
-var uglify = require("gulp-uglify"); //
-var sass = require("gulp-sass"); //sass 컴파일
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var minifyCSS = require('gulp-minify-css');
+var sass = require("gulp-sass");
+var sourcemaps = require('gulp-sourcemaps');
+var markdown = require('gulp-markdown');
 
+/*파일 경로*/
 var src = {};
-src.root = "/public/";
+var path = {};
+src.root = "public";
+src.dist = src.root + "/dist";
+src.scss = src.root + "/scss";
+src.js = src.root + "/js";
+src.css = src.dist + "/css";
+path.scss = src.scss + "/*.scss";
+path.js = src.js + "/*.js";
+path.css = src.css + "/*.css";
+
+
+//컴파일
+gulp.task("scss:compile", function () {
+    return gulp.src(path.scss)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(src.css));
+});
+
+//컨캣
+gulp.task("css:concat", function () {
+    return gulp.src(path.css)
+        .pipe(concat("main.css"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(src.css));
+});
+
+//와치
+gulp.task("watch", function () {
+    gulp.watch(path.scss, ["scss:compile"]);
+    gulp.watch(path.css, ["css:concat"]);
+});
+
+//마크다운
+gulp.task('md', function () {
+    return gulp.src("*.md")
+        .pipe(markdown())
+        .pipe(gulp.dest(src.dist));
+});
+
+gulp.task("default", ["scss:compile","css:concat", "watch"]);
---
+++ public/dist/text.md
@@ -0,0 +1,13 @@
+<h1 id="포트폴리오">포트폴리오</h1>
+<h2 id="첫번째">첫번째</h2>
+<p>반응형 (media query)</p>
+<ol>
+<li>640</li>
+<li>~480</li>
+<li>~414</li>
+<li>~375</li>
+<li>~320</li>
+</ol>
+<p>gulp
+scss
+jQuery</p>
