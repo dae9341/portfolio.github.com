30036c04575abc751104279c1523d450fc97ad2f
kdh976 <dae9341@gmail.com>
2019-05-08 17:28:34 +0900

    구글시트 연동

--- public/js/com/dataload.js
+++ public/js/com/dataload.js
@@ -1,4 +1,16 @@
-var aaa = $.getJSON("public/test.json", function(data) {
-    console.log(data);
-    console.log(data.name);
+var url = "https://sheets.googleapis.com/v4/spreadsheets/1bafTzBf62qEk72hBN7l1pSxgdYapzlMnZxvvPVmudVc/values/시트1";
+var param = {"key":"AIzaSyAdvMK0V-SuuDKa6PYuqg3Xz33JR4Pwg-4"};
+
+$.ajax({
+    url: url,
+    method:"GET",
+    data: param,
+    dataType:"json",
+    cache: false,
+    async:true
+    , success: function (data, status, xhr) {
+        console.log(data);
+    }, error: function (jqXHR, textStatus, errorThrown) {
+        console.log(arguments);
+    }
 });
